d27ba52e0fb7b7491368349be1bf0e59cccae0bf
Matheus Caetano <matheuseprofissional@gmail.com>
2025-06-22 15:47:22 -0300

    fix: packages

--- goalgorithms_test.go
+++ goalgorithms_test.go
@@ -1,14 +1,13 @@
-package goalgorithms_test
+package main_test
 
 import (
-	"goalgorithms"
 	"testing"
 )
 
 func TestBubbleSort(t *testing.T) {
 	arr := []int{64, 34, 25, 12, 22, 11, 90}
 	expected := []int{11, 12, 22, 25, 34, 64, 90}
-	sortedArr := goalgorithms.BubbleSort(arr)
+	sortedArr := main.BubbleSort(arr)
 	if !equal(sortedArr, expected) {
 		t.Errorf("BubbleSort failed: expected %v, got %v", expected, sortedArr)
 	}
